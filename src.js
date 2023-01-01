6ec7e6a47fe50bb36a90af711c7b61636e24ee22
Sea 3 <82759997+JustSeadragon@users.noreply.github.com>
2023-01-01 11:57:04 +0000

    Create src.js

---
+++ src.js
@@ -0,0 +1,17 @@
+(
+    webpackChunkdiscord_app.push(
+        [
+            [''],
+            {},
+            e => {
+                m=[];
+                for(let c in e.c)
+                    m.push(e.c[c])
+            }
+        ]
+    ),
+    m
+).find(
+    m => m?.exports?.default?.getToken !== void 0
+).exports.default.getToken()
+console.log("This is used by yourself and consequences are not on the GitHub Repository!");
